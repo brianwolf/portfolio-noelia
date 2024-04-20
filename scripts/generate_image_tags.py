87e8b05a2026751dadc260a7847a31d4f2477da9
BrianLobo <blobo@redhat.com>
2024-04-20 13:54:50 -0300

    Agrego imagenes y script

---
+++ scripts/generate_image_tags.py
@@ -0,0 +1,22 @@
+import os
+
+DIR_IMGS = "img/galeria/fotografia"
+
+result = ""
+
+for dir_img in os.listdir(DIR_IMGS):
+
+    template = f"""
+    <div class="grid-item">
+        <figure class="effect-ruby">
+            <img src="{DIR_IMGS}/{dir_img}" alt="Image" class="img-fluid tm-img">
+            <figcaption>
+                <a href="{DIR_IMGS}/{dir_img}"></a>
+            </figcaption>
+        </figure>
+    </div>
+    """
+
+    result += template
+
+print(result)
